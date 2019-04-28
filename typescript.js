dfa59c34076e759a3ff080b7e2335a1010529a59
Manuel Bieh <github@manuelbieh.de>
2019-04-28 22:18:20 +0100

    added internal to import/order

---
+++ typescript.js
@@ -0,0 +1,40 @@
+module.exports = {
+  overrides: [
+    {
+      files: ['** /*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+        warnOnUnsupportedTypeScriptVersion: true,
+      },
+      plugins: ['@typescript-eslint'],
+      rules: {
+        '@typescript-eslint/no-angle-bracket-type-assertion': 'warn',
+        'no-array-constructor': 'off',
+        '@typescript-eslint/no-array-constructor': 'warn',
+        '@typescript-eslint/no-namespace': 'error',
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            args: 'none',
+            ignoreRestSiblings: true,
+          },
+        ],
+        'no-useless-constructor': 'off',
+        '@typescript-eslint/no-useless-constructor': 'warn',
+      },
+    },
+    {
+      files: ['**/*.d.ts'],
+      rules: {
+        'import/export': 0,
+        'no-redeclare': 0,
+      },
+    },
+  ],
+};
